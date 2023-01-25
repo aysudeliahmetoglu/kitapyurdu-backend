4344ef56f31d9ac0d7d598a8e2e85f3ca07e8954
aysudeliahmetoglu <aysudeliahmetoglu@gmail.com>
2023-01-25 20:00:57 +0300

    user endpoints were added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  // @Get()
+  // getHello(): Promise<string> {
+  //   return this.appService.getHello();
+  // }
+
+  @Post("create-user")
+  async createUser(@Body() createCatDto: any) {
+    return this.appService.createUser(createCatDto);
+  }
+  @Get("user-by-email")
+  async getUserByEmail(@Body() createCatDto: any) {
+    return this.appService.getUserByEmail(createCatDto);
   }
 }
+
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,33 @@
 import { Injectable } from '@nestjs/common';
-
+import { PrismaClient, User } from '@prisma/client';
+const prisma = new PrismaClient();
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async createUser(data): Promise<User> {
+   try {
+    const newUser = await prisma.user.create({
+      data: {
+        name: 'Alice2',
+        email: data.email,
+      },
+    });
+    return newUser;
+   } catch (error) {
+    return error
+     
+   }
   }
+  async getUserByEmail(data): Promise<User> {
+    try {
+     const newUser = await prisma.user.findUnique({
+       where: {
+         email: data.email,
+       },
+     });
+     return newUser;
+    } catch (error) {
+     return error
+      
+    }
+   }
 }
