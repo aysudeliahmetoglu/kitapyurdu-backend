9bbeb68a9ad7ea66a50927f34acea6d3aeca190f
aysudeliahmetoglu <aysudeliahmetoglu@gmail.com>
2023-01-25 20:54:14 +0300

    new endpoint were added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -18,5 +18,37 @@ export class AppController {
   async getUserByEmail(@Body() createCatDto: any) {
     return this.appService.getUserByEmail(createCatDto);
   }
+  @Get("books/products")
+  async bookProducts(@Body() createCatDto: any) {
+    return this.appService.getBookProducts();
+  }
+  @Get("books/related")
+  async relatedBook(@Body() createCatDto: any) {
+    return this.appService.relatedBook();
+  }
+  @Get("books/kid")
+  async kidBook(@Body() createCatDto: any) {
+    return this.appService.kidBook();
+  }
+  @Get("books/best")
+  async bestSeller(@Body() createCatDto: any) {
+    return this.appService.bestSeller();
+  }
+  @Get("books/new")
+  async newBook(@Body() createCatDto: any) {
+    return this.appService.newBook();
+  }
+  @Get("books/global")
+  async globalBook(@Body() createCatDto: any) {
+    return this.appService.globalBook();
+  }
+  @Get("books/exam")
+  async examBook(@Body() createCatDto: any) {
+    return this.appService.examBook();
+  }
+  @Get("books/game-stuff")
+  async gameAndStuff(@Body() createCatDto: any) {
+    return this.appService.gameAndStuff();
+  }
 }
 
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,33 +1,95 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaClient, User } from '@prisma/client';
+import { Book, PrismaClient, User } from '@prisma/client';
 const prisma = new PrismaClient();
 @Injectable()
 export class AppService {
   async createUser(data): Promise<User> {
-   try {
-    const newUser = await prisma.user.create({
-      data: {
-        name: 'Alice2',
-        email: data.email,
-      },
-    });
-    return newUser;
-   } catch (error) {
-    return error
-     
-   }
+    try {
+      const newUser = await prisma.user.create({
+        data: {
+          name: 'Alice2',
+          email: data.email,
+        },
+      });
+      return newUser;
+    } catch (error) {
+      return error;
+    }
   }
   async getUserByEmail(data): Promise<User> {
     try {
-     const newUser = await prisma.user.findUnique({
-       where: {
-         email: data.email,
-       },
-     });
-     return newUser;
+      const newUser = await prisma.user.findUnique({
+        where: {
+          email: data.email,
+        },
+      });
+      return newUser;
     } catch (error) {
-     return error
-      
+      return error;
     }
-   }
+  }
+  async relatedBook(): Promise<any> {
+    try {
+      const books = await prisma.relatedBook.findMany();
+      return books;
+    } catch (error) {
+      return error;
+    }
+  }
+  async kidBook(): Promise<any> {
+    try {
+      const books = await prisma.kidBook.findMany();
+      return books;
+    } catch (error) {
+      return error;
+    }
+  }
+  async bestSeller(): Promise<any> {
+    try {
+      const books = await prisma.bestSeller.findMany();
+      return books;
+    } catch (error) {
+      return error;
+    }
+  }
+  async newBook(): Promise<any> {
+    try {
+      const books = await prisma.newBook.findMany();
+      return books;
+    } catch (error) {
+      return error;
+    }
+  }
+  async globalBook(): Promise<any> {
+    try {
+      const books = await prisma.globalBook.findMany();
+      return books;
+    } catch (error) {
+      return error;
+    }
+  }
+  async examBook(): Promise<any> {
+    try {
+      const books = await prisma.examBook.findMany();
+      return books;
+    } catch (error) {
+      return error;
+    }
+  }
+  async gameAndStuff(): Promise<any> {
+    try {
+      const books = await prisma.gameAndStuff.findMany();
+      return books;
+    } catch (error) {
+      return error;
+    }
+  }
+  async getBookProducts(): Promise<any> {
+    try {
+      const books = await prisma.bookPoduct.findMany();
+      return books;
+    } catch (error) {
+      return error;
+    }
+  }
 }
